9fb46386c76ecb620cda94a94c11a53e57abfd36
Tino Calancha <tino.calancha@gmail.com>
2020-12-11 17:26:43 +0100

    Rename to feature-verify-all-scenarios-touching-region
    This command do not restrict to scenarios inside the region; in fact,
    every scenario interseecting the region is included.
    
    Fpr convenience, we consider that an scenario that just starts
    at the end of the region, it is intersecting it.
    * feature-mode.el (feature-verify-all-scenarios-touching-region):
    Rename from feature-verify-all-scenarios-inside-region.

--- feature-mode.el
+++ feature-mode.el
@@ -70,6 +70,9 @@
 ;;  \C-c ,v
 ;;  :   Verify all scenarios in the current buffer file.
 ;;
+;;  \C-c ,w
+;;  :   Verify all scenarios touching the active region.
+;;
 ;;  \C-c ,s
 ;;  :   Verify the scenario under the point in the current buffer.
 ;;
@@ -280,7 +283,7 @@
   (setq feature-mode-map (make-sparse-keymap))
   (define-key feature-mode-map "\C-m" 'newline)
   (define-key feature-mode-map  (kbd "C-c ,s") 'feature-verify-scenario-at-pos)
-  (define-key feature-mode-map  (kbd "C-c ,w") 'feature-verify-all-scenarios-in-region)
+  (define-key feature-mode-map  (kbd "C-c ,w") 'feature-verify-all-scenarios-touching-region)
   (define-key feature-mode-map  (kbd "C-c ,v") 'feature-verify-all-scenarios-in-buffer)
   (define-key feature-mode-map  (kbd "C-c ,f") 'feature-verify-all-scenarios-in-project)
   (define-key feature-mode-map  (kbd "C-c ,g") 'feature-goto-step-definition)
@@ -736,11 +739,11 @@ Throw an error if there is no scenario at point."
    (list "-l" (number-to-string (line-number-at-pos)))
    :feature-file (feature-buffer-file-name)))
 
-(defun feature-scenarios-in-region (start end)
-  "Return the line of the title per each scenario inside the active region.
+(defun feature-scenarios-touching-region (start end)
+  "Return the line of the title per each scenario touching the active region.
 
-A scenario only partially inside the region is also included.
-We also include a Scenario if just starts at END."
+A scenario only partially inside the region is included.
+We also include a Scenario that just starts at END."
   (let* ((start (min start end))
          (end (max start end))
          (lines))
@@ -754,12 +757,12 @@ We also include a Scenario if just starts at END."
           (push (line-number-at-pos pos) lines)))
       (nreverse lines))))
 
-(defun feature-verify-all-scenarios-in-region (&optional start end)
-  "Run all the scenarios inside the selected region."
+(defun feature-verify-all-scenarios-touching-region (&optional start end)
+  "Run all the scenarios touching the selected region."
   (interactive "r")
   (feature-run-cucumber
    (list (mapconcat (lambda (line) (format "-l %d" line))
-              (feature-scenarios-in-region start end) " "))
+              (feature-scenarios-touching-region start end) " "))
    :feature-file (feature-buffer-file-name)))
 
 (defun feature-verify-all-scenarios-in-buffer ()
